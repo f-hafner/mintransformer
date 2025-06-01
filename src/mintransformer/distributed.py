3e65a10e5e62fba39236c1ec7571bebd8f8358e8
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2025-06-01 21:43:00 +0200

    fix wrong arg name

--- src/mintransformer/distributed.py
+++ src/mintransformer/distributed.py
@@ -13,5 +13,5 @@ def ddp_setup(rank: int, world_size: int, backend: str = "gloo") -> None:
     """
     os.environ["MASTER_ADDR"] = "localhost" # for single machine
     os.environ["MASTER_PORT"] = "12355" # "any free port" -- how do I see them? TODO -- use correct port
-    init_process_group(backend=dist_backend, rank=rank, world_size=world_size)
+    init_process_group(backend=backend, rank=rank, world_size=world_size)
 
